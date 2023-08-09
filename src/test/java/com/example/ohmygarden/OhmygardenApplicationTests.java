7a11fa05ea2d362b62ede627d7b20c1c87689911
osohyun0224 <osohyun0224@naver.com>
2023-08-09 15:23:46 +0900

    Feat: 스프링 부트 기본 세팅

---
+++ src/test/java/com/example/ohmygarden/OhmygardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ohmygarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhmygardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
